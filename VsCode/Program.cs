b023bbe0bd669480c7787af857585b006c60952e
P0mmi <mateo.pommier@ucb.edu.bo>
2024-08-12 12:00:29 -0400

    comic

--- VsCode/Program.cs
+++ VsCode/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace VsCode;
+﻿using System.Diagnostics.Contracts;
+
+namespace VsCode;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void CalcularEdad()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Decime que año naciste guapo/guapa");
+        string? anioNacimientoInput = Console.ReadLine();
+        if (string.IsNullOrEmpty(anioNacimientoInput))
+        {
+            Console.WriteLine("No me la charles pone un año de vrd");
+        }
+        else 
+        {
+            int anioNacimiento = Int32.Parse(anioNacimientoInput);
+            Console.WriteLine("Tenes: "+ (2024-anioNacimiento));
+        }
     }
+
 }

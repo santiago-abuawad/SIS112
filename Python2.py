1aa2aa241416e342b959caf5779245e07258e7a9
P0mmi <mateo.pommier@ucb.edu.bo>
2024-08-16 10:30:05 -0400

    PYTHON PORFIIIIIIN FUUUUCK

---
+++ Python2.py
@@ -0,0 +1,12 @@
+#HOLA MUNDO
+print("Hola Mundo")
+
+#OBTENER EDAD
+edad = input("ingresa tu edad: ")
+print(f"Tienes: {edad}")
+
+#CALCULAR EDAD
+calcluarEdad = int(input("Ingresa tu fecha de nacimiento: "))
+
+fechadeNacimiento = 2024-calcluarEdad
+print(f"tienes: {fechadeNacimiento}")

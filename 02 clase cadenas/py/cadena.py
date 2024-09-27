cda4f443ec3a86b4346d20730b4c5a37ae584798
P0mmi <mateo.pommier@ucb.edu.bo>
2024-09-27 10:13:54 -0400

    clase cadena en python

--- 02 clase cadenas/py/cadena.py
+++ 02 clase cadenas/py/cadena.py
@@ -29,8 +29,46 @@ class Cadena:
     def longitud(self):  # Obtiene la cantidad de caracteres que tiene la cadena
         return len(self.cadena)
 
-    def a_mayusculas(self):  # Obtiene la cadena transformada en MAYUSCULA
+    def a_mayusculas(self):  # Obtiene la cadena transformada en MAYÚSCULA
         return self.cadena.upper()
 
-    def a_minusculas(self):  # Obtiene la cadena transformada en minuscula
+    def a_minusculas(self):  # Obtiene la cadena transformada en minúscula
         return self.cadena.lower()
+
+# Ejemplo de uso
+cadena = Cadena('')
+
+def ejecutar(metodo, valor_input=''):
+    resultado = ''
+    if metodo == 'getCadena':
+        resultado = f'Cadena: {cadena.get_cadena()}'
+    elif metodo == 'concatenar':
+        cadena.concatenar(valor_input)
+        resultado = f'Cadena actual: {cadena.get_cadena()}'
+    elif metodo == 'invertir':
+        cadena.invertir()
+        resultado = f'Cadena invertida: {cadena.get_cadena()}'
+    elif metodo == 'esVacia':
+        resultado = f'¿Es vacía?: {cadena.es_vacia()}'
+    elif metodo == 'contiene':
+        resultado = f'¿Contiene "{valor_input}"?: {cadena.contiene(valor_input)}'
+    elif metodo == 'comienzaCon':
+        resultado = f'¿Comienza con "{valor_input}"?: {cadena.comienza_con(valor_input)}'
+    elif metodo == 'terminaCon':
+        resultado = f'¿Termina con "{valor_input}"?: {cadena.termina_con(valor_input)}'
+    elif metodo == 'longitud':
+        resultado = f'Longitud: {cadena.longitud()}'
+    elif metodo == 'aMayusculas':
+        resultado = f'A mayúsculas: {cadena.a_mayusculas()}'
+    elif metodo == 'aMinusculas':
+        resultado = f'A minúsculas: {cadena.a_minusculas()}'
+    else:
+        resultado = 'Método no reconocido.'
+
+    return resultado
+
+# Ejemplo de uso
+print(ejecutar('concatenar', 'Hola '))
+print(ejecutar('getCadena'))
+print(ejecutar('invertir'))
+print(ejecutar('longitud'))

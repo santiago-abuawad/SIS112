abb95126a1ca2da8e8de2493dcd33101972e04e8
P0mmi <mateo.pommier@ucb.edu.bo>
2024-10-04 10:45:04 -0400

    avance clase 04/10/2024

--- 03 calse lista de enteros/js/lista-enteros.js
+++ 03 calse lista de enteros/js/lista-enteros.js
@@ -1,52 +1,109 @@
-class ListaEnteros {
+class listaEnteros {
     constructor() {
         this.lista = [];
     }
 
     agregar(valor) {
-        this.lista.push(valor);
+        this.lista.push(parseInt(valor)); // Convertir a número entero
     }
 
     eliminar(valor) {
-        const index =this.lista.indexOf(valor);
+        const index = this.lista.indexOf(parseInt(valor)); // Convertir a número entero
         if (index !== -1) {
-            this.lista.splice(index,1);
+            this.lista.splice(index, 1);
         }
     }
 
     buscar(valor) {
-        return this.lista.indexOf(valor);
-    }  
+        return this.lista.indexOf(parseInt(valor)); // Convertir a número entero
+    }
 
     ordenar() {
-        this.lista.sort((a,b) => a - b); //orden ascendente
+        this.lista.sort((a, b) => a - b);
     }
 }
 
-const listaEnteros = new ListaEnteros();
-const input = document.getElementById('numeroInput');
-const agregarBtn = document.getElementById('agregarBtn');
-const listaNumeros = document.getElementById('listaNumeros');
-const eliminarBtn = document.getElementById('EliminarBtn');
-const BuscarUnDato = document.getElementById('BuscarlosDatos');
-const OrdenarDato = document.getElementById('OrdenarDato');
+// Crear una instancia de listaEnteros
+let miLista = new listaEnteros();
+miLista.agregar(21);
+miLista.agregar(12);
+miLista.agregar(15);
+miLista.agregar(30);
 
-agregarBtn.addEventListener('click', () => {
-    const valor = parseInt(input.value);
+// Mostrar el tercer elemento en la lista
+var a = miLista.lista[0];
+console.log(a); 
+
+var b = miLista.lista[1];
+console.log(b); 
+
+var c = miLista.lista[2];
+console.log(c); 
+
+var d = miLista.lista[3];
+console.log(d); 
+
+var datoMenor = -1;
+/*
+var a = miLista.lista[0];
+if(a > datoMenor){
+    datoMenor = a;
+}
+console.log("Dato Menor: " + datoMenor);
+
+var b = miLista.lista[1];
+if(a > b){
+    datoMenor = b;
+}
+console.log("Dato Menor: " + datoMenor);
+
+var c = miLista.lista[2];
+if(b > c){
+    datoMenor = c;
+}
+console.log("Dato Menor: " + datoMenor);
+
+var d = miLista.lista[3];
+if(c > d){
+    datoMenor = d;
+}
+console.log("Dato Menor: " + datoMenor);
+*/
+var datoMenor = miLista.lista[0]; // Asumimos que el primer elemento es el menor inicialmente
+
+for (let i = 1; i < miLista.lista.length; i++) {
+    if (miLista.lista[i] < datoMenor) {
+        datoMenor = miLista.lista[i];
+    }
+    console.log("Dato Menor: " + datoMenor);
+}
+// Función para agregar enteros a la lista y mostrarla
+function agregarEntero(valor) {
     if (!isNaN(valor)) {
-        listaEnteros.agregar(valor);
-        mostrarLista();
-        input.value = '';
+        miLista.agregar(valor);
+        console.log('Lista actualizada:', miLista.lista);
     } else {
-        alert('Por favor, ingresa un número válido.');
+        console.log('Por favor, ingrese un número válido.');
     }
-});
-
-function mostrarLista() {
-    listaNumeros.innerHTML = '';
-    listaEnteros.lista.forEach(num => {
-        const li = document.createElement('li');
-        li.textContent = num;
-        listaNumeros.appendChild(li);
-    });
 }
+
+// Función para buscar un entero en la lista
+function buscarEntero(valor) {
+    const index = miLista.buscar(valor);
+    if (index !== -1) {
+        console.log(valor + ' se encuentra en la posición: ' + index);
+    } else {
+        console.log(valor + ' no se encuentra en la lista.');
+    }
+}
+
+// Función para ordenar la lista
+function ordenarLista() {
+    miLista.ordenar();
+    console.log('Lista ordenada:', miLista.lista);
+}
+
+// Ejemplo de uso en la consola
+agregarEntero(45); // Agregar un número
+buscarEntero(12);  // Buscar un número
+ordenarLista();    // Ordenar la lista
